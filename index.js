6df1d1e93c6157cd9ffb35c44bffabcdc5beb4e1
razzak118348 <abdurrazzak118348@gmail.com>
2024-11-06 19:45:09 +0600

    final

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 const carftCollection = client.db('artAndCraftDB').collection('allCraft');
 
@@ -101,7 +101,7 @@ app.delete('/allcraft/:id',async(req,res)=>{
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -115,6 +115,6 @@ app.get('/',(req,res)=>{
     res.send('Art and Craft server is running')
 })
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`server is running on port ${port}`)
+// })
